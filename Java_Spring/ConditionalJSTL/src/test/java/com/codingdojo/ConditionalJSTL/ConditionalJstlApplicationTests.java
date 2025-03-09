43eb0069ffd43f85373958d5d4855295fba5483c
vphan278 <vphan2662@gmail.com>
2025-03-09 12:40:37 -0700

    Add files via upload

---
+++ Java_Spring/ConditionalJSTL/src/test/java/com/codingdojo/ConditionalJSTL/ConditionalJstlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ConditionalJSTL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalJstlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
