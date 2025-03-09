95693a14a5d85a302c25788ecc171c885769e880
vphan278 <vphan2662@gmail.com>
2025-03-09 12:39:41 -0700

    Add files via upload

---
+++ ConditionalJSTL/src/main/java/com/codingdojo/ConditionalJSTL/ConditionalJstlApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ConditionalJSTL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalJstlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalJstlApplication.class, args);
+	}
+
+}
